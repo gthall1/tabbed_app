9e5e263cc40333322622c232e0fa3d247e7a16f3
Griff Hall <griffhall1@gmail.com>
2014-11-25 17:47:28 -0600

    Initial Commit

---
+++ tabbed_app/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  tabbed_app
+//
+//  Created by Griff Hall on 11/25/14.
+//  Copyright (c) 2014 Griffappolis. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ tabbed_app/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tabbed_app
+//
+//  Created by Griff Hall on 11/25/14.
+//  Copyright (c) 2014 Griffappolis. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
